3470b9c7264cb9788dfb054c770961102a055433
Bede Kelly <bedekelly97@gmail.com>
2014-10-21 23:39:47 +0100

    Convenience for splitting arguments up

---
+++ pysh.py
@@ -0,0 +1,20 @@
+"""
+PySh
+A tiny, intuitive interface to access shell commands as if they were functions
+in Python.
+
+Examples:
+    >>> from sh import sh
+    >>> sh.cp("file1", "file2")
+    >>> sh.mv("file3", "file4", "file5", "directoryA")
+    >>> sh.ls("--color=auto")
+        ... ls output ...
+    >>>
+    >>> sh.asdfghjkl()
+    pysh: asdfghjkl: command not found
+    
+"""
+
+# Dummy program so as not to clutter the namespace.
+from lib.handler import ShellHandler as _ShellHandler
+sh = _ShellHandler()
