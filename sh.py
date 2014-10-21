5be1c2baf6c26c601470c8a994792c2b8715b78a
Bede Kelly <bedekelly97@gmail.com>
2014-10-21 23:19:13 +0100

    Implementation, readme

--- sh.py
+++ sh.py
@@ -1,1 +1,15 @@
-from _sh import sh
+"""
+PySh
+A tiny, intuitive interface to access shell commands as if they were functions
+in Python.
+
+Examples:
+    from sh import sh
+    sh.cp("file1", "file2")
+    sh.mv("file3", "file4", "file5", "directoryA")
+    sh.ls("--color=auto")
+"""
+
+# Dummy program so as not to clutter the namespace.
+from lib.handler import ShellHandler as _ShellHandler
+sh = _ShellHandler()
